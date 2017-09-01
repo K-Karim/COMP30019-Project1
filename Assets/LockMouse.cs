36093d2d0e14ed3a8df117975872b49268c43102
Karim Khairat <kkhairat@student.unimelb.edu.au>
2017-09-01 11:42:03 +1000

    Added comments.

--- Assets/LockMouse.cs
+++ Assets/LockMouse.cs
@@ -1,5 +1,5 @@
 ﻿//Code from https://docs.unity3d.com/ScriptReference/Cursor-lockState.html
-
+// Adapted for COMP30019 by Brody Taylor, Karim Khairat and Duy (Daniel) Vu
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -18,17 +18,18 @@ public class LockMouse : MonoBehaviour {
 	}
 
 	void Start(){
+		// to start we want a locked cursor and invisible cursor
 		wantedMode = CursorLockMode.Locked;
 		SetCursorState ();
 	}
 
 	void Update(){
-		
+		//Pressing escape allows you to unlock cursor.
 		if (Input.GetKeyDown (KeyCode.Escape)) {
 			wantedMode = CursorLockMode.None;
 			SetCursorState ();
 		}
-
+		//Left click allows you to (re-)lock cursor if it has been unlocked
 		if (Input.GetMouseButtonDown (0)) {
 			wantedMode = CursorLockMode.Locked;
 			SetCursorState ();
